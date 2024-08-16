dcbe257a8c360debe72903932307d0056d446283
Jonathan Urias <ih8merta@gmail.com>
2024-08-16 14:54:22 -0400

    commented out functino call, messed with imports in other modules

--- set1/singleByteXORCipher/singleByteXORCipher.js
+++ set1/singleByteXORCipher/singleByteXORCipher.js
@@ -31,6 +31,6 @@ main = (encoded) => {
 	}
 }
 
-main("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"); // Returns "Cooking MC's like a pound of bacon" for the top result
+// main("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"); // Returns "Cooking MC's like a pound of bacon" for the top result
 
 module.exports = singleByteXORCipher;
